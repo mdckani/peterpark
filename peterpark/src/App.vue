3b5259f345cf5747856bb3a787dc044bc0587e91
mdckani <kani.kd.kani@gmail.com>
2022-07-19 23:36:03 +0200

    Table with mockdata and search and sort implemented

--- peterpark/src/App.vue
+++ peterpark/src/App.vue
@@ -1,22 +1,169 @@
 <template>
-  <div >
-    <h1 >Peter Park</h1>
-    <h2  >Long term parking permits</h2>
-    
-      <Table></Table>
-  
+  <div class="container text-center mt-5 mb-5">
+    <h1 class="mt-5 fw-bolder text-success">Peter Park</h1>
+    <h2 class="mt-5 fw-bolder text-warning">Long term parking permits</h2>
+    <div class="table-responsive my-5">
+      <Table
+        :fields="fields"
+        :permits="permits"
+        :countries="countries"
+        :onchange="onchange"
+      ></Table>
+    </div>
   </div>
 </template>
 
 <script>
 import Table from "./components/Table.vue";
- 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export default {
   name: "App",
   components: {
     Table,
-  }, 
+  },
+  setup() {
+    // Germany ^[A-ZÄÖÜ]{1,3}\-[ ]{0,1}[A-Z]{0,2}[0-9]{1,4}[H]{0,1}
+    // France   /^(?!ss|ww|.[iou]|[iou].)[a-z]{2}[-\s]?\d{3}[-\s]?(?!ss|ww|.[iou]|[iou].)[a-z]{2}$/i
+    //
+    //An array of values for the data
+    const permits = [
+      {
+        licensePlate: "F-RM373",
+        country: "DE",
+        startDate: "2022-02-23",
+        endDate: "2022-02-23",
+        ownerName: "Kanimozhi Duraikannu",
+        status: true,
+      },
+      {
+        licensePlate: "AB-123-CD",
+        country: "FR",
+        startDate: "2022-02-23",
+        endDate: "2022-02-23",
+        ownerName: "Nivedha Duraikannu",
+        status: true,
+      },
+      {
+        licensePlate: "DE12324",
+        country: "AU",
+        startDate: "2022-02-23",
+        endDate: "2022-02-23",
+        ownerName: "Clemens Mirbach",
+        status: false,
+      },
+      {
+        licensePlate: "WÜ-Z1",
+        country: "SW",
+        startDate: "2022-02-23",
+        endDate: "2022-02-23",
+        ownerName: "Kanimozhi Duraikannu",
+        status: true,
+      },
+      {
+        licensePlate: "DE12324",
+        country: "DE",
+        startDate: "2022-02-23",
+        endDate: "2022-02-23",
+        ownerName: "Meenatchi Duraikannu",
+        status: true,
+      },
+      {
+        licensePlate: "FE5467",
+        country: "FR",
+        startDate: "2022-02-23",
+        endDate: "2022-02-23",
+        ownerName: "Duraikannu",
+        status: false,
+      },
+      {
+        licensePlate: "FFSW234",
+        country: "SW",
+        startDate: "2022-02-23",
+        endDate: "2022-02-23",
+        ownerName: "Cedrice",
+        status: true,
+      },
+      {
+        licensePlate: "DE12324",
+        country: "AU",
+        startDate: "2022-02-23",
+        endDate: "2022-02-23",
+        ownerName: "Clemens Mirbach",
+        status: true,
+      },
+      {
+        licensePlate: "TTTT",
+        country: "FR",
+        startDate: "2022-02-23",
+        endDate: "2022-02-23",
+        ownerName: "Kanimozhi Duraikannu",
+        status: true,
+      },
+    ];
+
+    const fields = [
+      {
+        label: "License Plate",
+        property: "licensePlate",
+        columnType: "input",
+        onchange: "onChange()",
+      },
+      {
+        label: "Country",
+        property: "country",
+        columnType: "country",
+        onchange: "onChange",
+      },
+      {
+        label: "Start Date",
+        property: "startDate",
+        columnType: "date",
+        onchange: "onChangeStartDate",
+      },
+      {
+        label: "End Date",
+        property: "endDate",
+        columnType: "date",
+        onchange: "onChangeEndDate",
+      },
+      {
+        label: "Owner Name",
+        property: "ownerName",
+        columnType: "ownerName",
+        onchange: "onChangeOwnerName($event)",
+      },
+      {
+        label: "Status",
+        property: "status",
+        columnType: "status",
+        onchange: "onChangeStatus",
+      },
+    ];
+    const countries = [
+      { value: "DE", label: "Germany" },
+      { value: "FR", label: "France" },
+      { value: "SW", label: "Switzerland" },
+      { value: "AU", label: "Austria" },
+      { value: "", label: "Other" },
+    ];
+    const onchange = (e, val) => {
+      console.log(e);
+      console.log(val);
+      console.log("Test");
+    };
+    return { permits, fields, countries, onchange };
+  },
 };
 </script>
 
- 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- peterpark/src/components/Table.vue
+++ peterpark/src/components/Table.vue
@@ -1,27 +1,133 @@
 <template>
-  <h2>Table</h2>
-  <table>
-    <thead>
+  <div class="searchBar">
+    <!-- Filter Search -->
+    <div class="input-group mb-5">
+      <input
+        type="search"
+        class="form-control"
+        v-model="searchQuery"
+        placeholder="Search by Number plate / Vechile owner name"
+        aria-label="Recipient's username"
+        aria-describedby="button-addon2"
+      />
+    </div>
+  </div>
+
+  <table id="tableComponent" class="table table-bordered table-striped">
+     <thead>
       <tr>
-        <th>Column1</th>
-        <th>Column2</th>
+        <!-- Loop through columns -->
+        <th
+          v-for="field in fields"
+          :key="field.property"
+          @click="sortTable(field.property)"
+        >
+          {{ field.label }}
+          <i class="bi bi-sort-alpha-down" aria-label="Sort Icon"></i>
+        </th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>Cell 11</td>
-        <td>Cell 12</td>
-      </tr>
-      <tr>
-        <td>Cell 11</td>
-        <td>Cell 12</td>
+      <!-- Loop through the rows -->
+      <tr v-for="item in filterItems" :key="item">
+        <td v-for="field in fields" :key="field.property">
+          <div v-if="field.columnType === 'date'">
+            <input
+              @change="onchange(item, field.property)"
+              type="date"
+              format="dd-MM-yyyy"
+              v-model="item[field.property]"
+            />
+          </div>
+          <div v-else-if="field.columnType === 'status'">
+            <input
+              @change="onchange(item, field.property)"
+              type="checkbox"
+              v-model="item[field.property]"
+            />
+          </div>
+          <div v-else-if="field.columnType === 'country'">
+            <select
+              @change="onchange(item, field.property)"
+              class="form-control"
+              :required="true"
+              v-model="item[field.property]"
+            >
+              <option
+                v-for="option in countries"
+                v-bind:value="option.value"
+                v-bind:key="option.value"
+              >
+                {{ option.label }}
+              </option>
+            </select>
+          </div>
+          <div v-else>
+            <input
+              class="form-control"
+              type="text"
+              v-model="item[field.property]"
+              @change="onchange(item, field.property)"
+            />
+          </div>
+        </td>
       </tr>
     </tbody>
   </table>
 </template>
 <script>
+import { computed, ref } from "vue";
+import { sortBy } from "lodash";
+
 export default {
   name: "TableComponent",
+  props: {
+    permits: {
+      type: Array,
+    },
+    fields: {
+      type: Array,
+    },
+    countries: {
+      type: Array,
+    },
+    onchange: {
+      type: Function,
+    },
+  },
+
+  setup(props) {
+    let sort = ref(false);
+    let updatedList = ref([]);
+    let searchQuery = ref("");
+
+    const sortTable = (col) => {
+      sort.value = !sort.value;
+      updatedList.value = sortBy(props.permits, col);
+    };
+
+    const sortItems = computed(() => {
+      if (sort.value) {
+        return updatedList.value;
+      } else {
+        return props.permits;
+      }
+    });
+    const filterItems = computed(() => {
+      return sortItems.value.filter((product) => {
+        return (
+          product.licensePlate
+            .toLowerCase()
+            .indexOf(searchQuery.value.toLowerCase()) != -1 ||
+          product.ownerName
+            .toLowerCase()
+            .indexOf(searchQuery.value.toLowerCase()) != -1
+        );
+      });
+    });
+
+    return { sortItems, sortTable, searchQuery, filterItems };
+  },
 };
 </script>
 <style scoped>
